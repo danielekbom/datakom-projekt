d6516bc3404d5936a357bd7b5a1bd93ccf41a48c
danielekbom <daniel5.2@hotmail.com>
2015-11-14 15:25:04 +0100

    ny player sprites och animation

--- client/entities.js
+++ client/entities.js
@@ -12,16 +12,34 @@ Player = function(x,y,width,height,velocity){
     self.moveUp = false;
     self.moveRight = false;
     self.moveDown = false;
+    self.direction = 2;
+    self.animationCounter = 0;
     
     self.update = function(delta){
         if(self.moveLeft) self.x -= self.velocity * delta;
         if(self.moveUp) self.y -= self.velocity * delta;
         if(self.moveRight) self.x += self.velocity * delta;
         if(self.moveDown) self.y += self.velocity * delta;
+
+        self.animationCounter += 0.1;
     };
     
     self.draw = function(){
-        ctx.drawImage(Img.player,0,0,32,32,self.x,self.y,self.width,self.height);
+        var currentSprite = Math.floor(self.animationCounter) % 3;
+        
+        if(self.moveLeft){
+            self.direction = 3;
+        }else if(self.moveUp){
+            self.direction = 0;
+        }else if(self.moveRight){
+            self.direction = 1;
+        }else if(self.moveDown){
+            self.direction = 2;
+        }else{
+            currentSprite = 1;
+            self.animationCounter = 0;
+        }
+        ctx.drawImage(Img.player,48*currentSprite,52*self.direction,52,52,self.x,self.y,self.width,self.height);
     };
     
     return self;
