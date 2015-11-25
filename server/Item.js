f1326a97bad19d1eb69a24851bfb1b62c46b215c
Daniel <denial93@gmail.com>
2015-11-25 16:57:50 +0100

    Item inheritance

--- server/Item.js
+++ server/Item.js
@@ -1,25 +1,45 @@
+var inheritsFrom = function (child, parent) {
+    child.prototype = Object.create(parent.prototype);
+};
+
 var ItemTypeEnum = {
   WEAPON: 1,
   POWERUP: 2,
 };
 
-var WEAPONSORTS = {
+var WeaponSorts= {
   SWORD: 1,
   AXE: 2,
 };
+console.log('Test ' + ItemTypeEnum.WEAPON);
 
-
-Item = function(itemType,sortName, image){
+/*var Item = function(itemType, image){
     var self = {
         itemType : itemType,
-        itemSort : sortName,
         imageSource : image
     };
     return self;
-}
+};*/
+var Item = function(itemType, image) {
+    this.itemType = itemType;
+    this.image = image;
+};
 
-WeaponItem = function(weaponSort){
-    var self = {
-        damage
-    }
-}
+
+Item.prototype.write = function() {
+    console.log(this.itemType);
+};
+
+var WeaponItem = function(weaponSort, image){
+    this.damage = 10;
+    this.itemSort = weaponSort;
+    
+};
+inheritsFrom(WeaponItem, Item);
+
+var item = new Item(ItemTypeEnum.WEAPON, "hig");
+item.write();
+var sword = new WeaponItem(ItemTypeEnum.WEAPON, "hdifhi");
+sword.protype.itemType = ItemTypeEnum.WEAPON;
+//inheritsFrom(sword, new Item(ItemTypeEnum.WEAPON, "image"));
+sword.write();
